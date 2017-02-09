5df86994b2be68885ebaf24313d658d645164f6e
zhengjinwei123 <2538698032@qq.com>
2017-02-09 16:31:48 +0800

    update

--- ES6/demo1.js
+++ ES6/demo1.js
@@ -12,6 +12,28 @@ class Point {
 }
 
 let a = new Point(1,2);
-console.log(a.toString())
+console.log(a.toString());
+
+
+if(true){
+    function f(){
+        console.log(1111111);
+    }
+
+    f();
+}
+
+const PI=3.14;
+//const PI2;
+
+//冻结常量对象
+const t = Object.freeze([1,2,3]);
+console.log(t);
+//t.push(4);
+console.log(PI);
+
+var d = 1;
+console.log(global.d)
+
 
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 学习 react 技术
 
 #ES6 语法的js脚本依赖于babel解析器,调用方法：bebel-node demo1.js
+
+#官方学习指南:https://facebook.github.io/react/docs/introducing-jsx.html
