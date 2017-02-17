178d348237d49f1605c59ccdd0f159b547cf95a6
zhengjinwei123 <2538698032@qq.com>
2017-02-17 19:02:01 +0800

    update

--- details/demo1/app.js
+++ details/demo1/app.js
@@ -79,3 +79,43 @@ ReactDOM.render(
     <Ignore />,
     document.getElementById("root5")
 );
+
+//refs
+class CustomTextInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.focus = this.focus.bind(this);
+    }
+    focus(){
+        this.textInput.focus();
+    }
+    render(){
+        return (
+            <div>
+                <input type="text" ref={(input)=>{this.textInput = input}}/>
+                <input type="button" value="focus the text input" onClick={this.focus}/>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <CustomTextInput />,
+    document.getElementById("root6")
+);
+
+class AutoFocusTextInput extends React.Component{
+    componentDidMount(){
+        this.textInput.focus();
+    }
+
+    render(){
+        return (
+          <CustomTextInput ref={(input)=>{this.textInput = input;}} />
+        );
+    }
+}
+
+ReactDOM.render(
+    <AutoFocusTextInput />,
+    document.getElementById("root7")
+);
